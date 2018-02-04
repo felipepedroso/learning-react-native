5d1a003ca5134d0c45e30514197a77d04136ae00
Felipe Pedroso <f.pedroso677@gmail.com>
2018-02-04 13:47:39 -0200

    Creating the header component.

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,15 @@
+// //  Original code from template
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('albums', () => App);
+
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header />
+);
 
 AppRegistry.registerComponent('albums', () => App);
