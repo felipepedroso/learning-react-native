7502288c5a5014a2ac7fe608fb701fb6d472870e
Felipe Pedroso <f.pedroso677@gmail.com>
2018-02-08 10:45:11 -0200

    Implementing a Buy button.

--- albums/src/components/AlbumItem.js
+++ albums/src/components/AlbumItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumItem = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
@@ -25,6 +26,11 @@ const AlbumItem = ({ album }) => {
                     style={imageStyle}
                     source={{ uri: image }}
                 />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy
+                </Button>
             </CardSection>
         </Card>
     );
