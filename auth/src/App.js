a3d9ff8d8a4affa5ab50e70be10567cc89d0af03
Felipe Pedroso <f.pedroso677@gmail.com>
2018-02-19 10:28:52 -0300

    Creating a form with user field.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDfjM7HNfWs-gO6VNJj6c2XmgXIOl6D6PY',
+            authDomain: 'authenticaton-3311e.firebaseapp.com',
+            databaseURL: 'https://authenticaton-3311e.firebaseio.com',
+            projectId: 'authenticaton-3311e',
+            storageBucket: 'authenticaton-3311e.appspot.com',
+            messagingSenderId: '885099817256'
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>An app!</Text>
+                <Header headerText="Authentication" />
+                <LoginForm />
             </View>
         );
     }
