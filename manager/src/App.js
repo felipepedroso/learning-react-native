914487639cea391c80e498f7047b0d303ef5e777
Felipe Pedroso <f.pedroso677@gmail.com>
2018-02-22 12:17:26 -0300

    Removing the generated App component and adding the Firebase credentials.

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCn49Yh8eUJ0YWj_kcX_Bscsne1ygvr5oU",
+            authDomain: "manager-b4dbb.firebaseapp.com",
+            databaseURL: "https://manager-b4dbb.firebaseio.com",
+            projectId: "manager-b4dbb",
+            storageBucket: "",
+            messagingSenderId: "354689771078"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
