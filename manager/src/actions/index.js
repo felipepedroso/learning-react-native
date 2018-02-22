18b6ab32aedf0d0800dc3486029dfd3cecbfe5ba
Felipe Pedroso <f.pedroso677@gmail.com>
2018-02-22 16:38:18 -0300

    Making the app navigate to employee list screen.

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
 
 export const emailChanged = (text) => {
@@ -37,4 +38,5 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+    Actions.employeeList();
 };
